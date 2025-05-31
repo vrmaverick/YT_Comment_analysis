49c1f2bb68987494b905887a305b5053ae8dfb8a
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2025-05-31 18:57:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     
     video_id = input("Enter id of yt video you wish to analyze 50 comments : ")
     max_result = 50  # Number of comments to retrieve (maximum 50 per API request)
-    api_key ="AIzaSyC_4xZTiNuz1O-Qu5kYnlg82riP30KRIxY"  # Your YouTube API key
+    api_key = # Your YouTube API key
     df = initialize(video_id,api_key,max_result)
 
     
